b82e746811eb6ec185b861b4cfdb3807469c56c5
D4NI017 <150013225+D4NI017@users.noreply.github.com>
2024-03-11 17:41:20 +0100

    Add files via upload

---
+++ Tarea2/src/es/iesmz/tests/DiasLluvia.java
@@ -0,0 +1,131 @@
+package es.iesmz.tests;
+
+public class DiasLluvia {
+    static boolean[][] calendario = new boolean[12][31];
+
+    static boolean registroDia(int dia, int mes, boolean lluvia){
+        if (dia >= 1 && dia <= 31 && mes > 1 && mes <= 12){
+            calendario [mes][dia] = lluvia;
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    static boolean consultarDia (int mes, int dia){
+        return calendario[mes][dia];
+    }
+
+    int contarDiasLluvia(){
+        int cantidad = 0;
+
+        for (int i = 0; i < calendario.length; i++) {
+            for (int j = 0; j < calendario.length; j++) {
+                if (calendario[i][j]){
+                    cantidad++;
+                }
+            }
+        }
+        return cantidad;
+    }
+
+    static int trimestreLluvioso(){
+        int trim1 = 0;
+        int trim2 = 0;
+        int trim3 = 0;
+        int trim4 = 0;
+
+        for (int i = 0; i < calendario.length; i++) {
+            for (int j = 0; j < calendario.length; j++) {
+                if (i < 3){
+                    if (calendario[i][j]){
+                        trim1++;
+                    }
+                }
+                else if (i < 6){
+                    if (calendario[i][j]){
+                        trim2++;
+                    }
+                }
+
+                else if (i < 9){
+                    trim3++;
+                }
+
+                else if (i < 12){
+                    trim4++;
+                }
+            }
+        }
+
+        if (trim1 > trim2 && trim1 > trim3 && trim1 > trim4){
+            return trim1;
+        } else if (trim2 > trim1 && trim2 > trim3 && trim2 > trim4){
+            return trim2;
+        } else if (trim3 > trim1 && trim3 > trim2 && trim3 > trim4){
+            return trim3;
+        }else {
+            return trim4;
+        }
+    }
+
+    static int primerDiaLluvia(int dia, int mes){
+        int diasTotales = 0;
+
+        for (int i = 0; i < calendario.length; i++) {
+            for (int j = 0; j < calendario.length; j++) {
+                if (calendario[mes][dia]){
+
+                    if (mes == 0){ //CALCULAR ENERO
+                        diasTotales = j;
+                    }
+
+                    if (mes == 1){ //CALCULAR FEBRERO
+                        diasTotales = 31 + j;
+                    }
+
+                    if (mes == 2 ){ //CALCULAR MARZO
+                        diasTotales = 31+ 28 + j;
+                    }
+
+                    if (mes == 3){ //CALCULAR ABRIL
+                        diasTotales = 28 + (31*2) + j;
+                    }
+
+                    if (mes == 4){ //CALCULAR MAYO
+                        diasTotales = 28 + (31 * 2) + 30 + j;
+                    }
+
+                    if (mes == 5){ //CALCULAR JUNIO
+                        diasTotales = 28 + (31 * 3) + 30 + j;
+                    }
+
+                    if (mes == 6){ //CALCULAR JULIO
+                        diasTotales = 28 + (31 * 3) + (30*2) + j;
+                    }
+
+                    if (mes == 7){ //CALCULAR AGOSTO
+                        diasTotales = 28 + (31 * 4) + (30*2) + j;
+                    }
+
+                    if (mes == 8){ //CALCULAR SEPTIEMBRE
+                        diasTotales = 28 + (31 * 4) + (30*3) + j;
+                    }
+
+                    if (mes == 9){ //CALCULAR OCTUBRE
+                        diasTotales = 28 + (31 * 4) + (30*4) + j;
+                    }
+
+                    if (mes == 10){ //CALCULAR NOVIEMBRE
+                        diasTotales = 28 + (31 * 5) + (30*4) + j;
+                    }
+
+                    if (mes == 11){ //CALCULAR DICIEMBRE
+                        diasTotales = 28 + (31 * 5) + (30*5) + j;
+                    }
+                }
+            }
+        }
+        return diasTotales;
+    }
+}
---
+++ Tarea2/src/es/iesmz/tests/DiasLluviaTest.java
@@ -0,0 +1,66 @@
+package es.iesmz.tests;
+
+import es.iesmz.tests.DiasLluvia;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DiasLluviaTest {
+    @Test
+    public void test1RegistroDia(){ //Test para registrar un dia que existe
+        boolean resultadoReal = DiasLluvia.registroDia(13, 5, true);
+        boolean resultadoEsperado = true;
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
+    @Test
+    public void test2RegistroDia(){ //Test para registrar un dia que existe
+        boolean resultadoReal = DiasLluvia.registroDia(22, 7, false);
+        boolean resultadoEsperado = true;
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
+    @Test
+    public void test3RegistroDia(){ //Test para registrar un dia que no existe
+        boolean resultadoReal = DiasLluvia.registroDia(43, 13, true);
+        boolean resultadoEsperado = true;
+        assertNotEquals(resultadoEsperado, resultadoReal);
+    }
+
+    @Test
+    public void test1consultarDia(){ //Test para consultar un dia que no se ha registrado como lluvia anteriormente
+        boolean resultadoReal = DiasLluvia.consultarDia(1, 3);
+        boolean resultadoEsperado = false;
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
+    @Test
+    public void test2consultarDia(){ //Test para consultar un dia que no se ha registrado como lluvia anteriormente
+        boolean resultadoReal = DiasLluvia.consultarDia(9, 23);
+        boolean resultadoEsperado = false;
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
+    @Test
+    public void test3consultarDia(){ //Test para consultar un dia que no se ha registrado como lluvia anteriormente y indicamos como resultado esperado True
+        boolean resultadoReal = DiasLluvia.consultarDia(7, 30);
+        boolean resultadoEsperado = true;
+        assertNotEquals(resultadoEsperado, resultadoReal);
+    }
+
+    //No se como realizar este test ya que no he creado un programa en el que registrar datos por lo tanto al no tener ningun dia
+    // como true no me da el resultado que quiero
+    @Test
+    public void test1trimestreLluvioso(){
+        int resultadoReal = DiasLluvia.trimestreLluvioso();
+        int resultadoEsperado = 1;
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
+    @Test
+    public void test1primerDiaDeLluvia(){
+        int resultadoReal = DiasLluvia.primerDiaLluvia(12,0);
+        int resultadoEsperado = 12;
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+}
